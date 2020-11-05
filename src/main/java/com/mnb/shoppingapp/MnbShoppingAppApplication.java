ebef7215e8bcf194b2867e238677b0d5ad145234
Merve Nurgül Bağcı <mervenurgulbagci@gmail.com>
2020-11-06 00:43:26 +0300

    Initial commit

---
+++ src/main/java/com/mnb/shoppingapp/MnbShoppingAppApplication.java
@@ -0,0 +1,13 @@
+package com.mnb.shoppingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnbShoppingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnbShoppingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnb/shoppingapp/MnbShoppingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnb.shoppingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnbShoppingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
